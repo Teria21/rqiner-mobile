e06d2bf82b562e4c17e37d05ab796e364c21a465
Teria21 <teriia123@outlook.com>
2024-10-17 02:31:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ apt install wget curl -y
 wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.0/rqiner-aarch64-mobile -O rqiner # fallback miner
 curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner
 chmod +x rqiner
-./rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l miner_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t 6
+./rqiner -i CHUTYOGWAVQRSGMTDWYXQCYJYQPBZTQRCILZYQUFICZJFQUMVGHZCWJAGGLE -l miner_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t 6
